6f9500c1011a55b183e9e44da2d2e2c8dc71a3e4
Nicolae Pop <nicupop729@gmail.com>
2021-10-26 21:18:54 -0300

    build the HTML, populate to-do list with JS, style with CSS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-import "./style.css";
+import './style.css';
+
+const listContainer = document.querySelector('.list');
+
+let tasks = [
+  {
+    description: 'to do the shopping',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'finish your Microverse project',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'bake bread',
+    completed: true,
+    index: 3,
+  },
+];
+
+const populateList = (values) => {
+  values.map((toDo, i) => {
+    const htmlText = `
+    <li class="item">
+      <input type="checkbox" class="${toDo.completed}" id="${toDo.description[0]}${toDo.index}"/>
+      <label for="${toDo.description[0]}${toDo.index}" class="item-description">${toDo.description}</label>
+      
+      <ion-icon name="ellipsis-vertical-outline" class="three-dots display-dots"></ion-icon>  
+    </li>`;
+
+    if (i + 1 === toDo.index) listContainer.innerHTML += htmlText;
+  });
+};
+
+populateList(tasks);
